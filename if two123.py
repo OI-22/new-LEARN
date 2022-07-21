20d1a7559a63e4be92cdf7942f65e51d394afcf3
OI-22 <oleksandr.ivanovich22@gmail.com>
2022-07-21 19:33:04 +0300

    nbhj

---
+++ if two123.py
@@ -0,0 +1,12 @@
+a = "Саша"
+b = "Саня"
+c = "Артем"
+while True :
+    guess = input('Кто моет посуду: ')
+    if guess == a:
+        print('Поздровляем вы угадали')
+        break
+    elif guess == b:
+        print('Подумайте лучше')
+    else:
+        print('Подумайте лучше')
